ee10f953d79fd3f9f9319817ebea6b78a7a778c4
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-01-03 17:21:45 +0100

    modifica file exec

--- fork/es2/exec.c
+++ fork/es2/exec.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main() {
-    execl("ls", "ls");
-    return 0;
+
+    int pid = fork();
+    
+    if(pid>0){
+        wait(NULL);
+        execl("/usr/bin/echo", "", "Ciao sono il processo genitore che usa ECHO", NULL);
+    }else if(pid==0){
+        execl("/usr/bin/echo", "", "Ciao sono il figlio del processo genitore che usa ECHO, e ho la precedenza", NULL);
+    }
+
+    //execl("/bin/ls", "ls", NULL);
+    return 0; 
 }
