bc78fe5815eee993e66166cc41452f558d8a20e1
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-01-03 23:44:29 +0100

    modificati alcuni files

--- fork/es2/exec.c
+++ fork/es2/exec.c
@@ -9,11 +9,10 @@ int main() {
     
     if(pid>0){
         wait(NULL);
-        execl("/usr/bin/echo", "", "Ciao sono il processo genitore che usa ECHO", NULL);
+        execl("/usr/bin/echo", "echo", "Ciao sono il processo genitore che usa ECHO", NULL);
     }else if(pid==0){
-        execl("/usr/bin/echo", "", "Ciao sono il figlio del processo genitore che usa ECHO, e ho la precedenza", NULL);
+        execl("/usr/bin/echo", "echo", "Ciao sono il figlio del processo genitore che usa ECHO, e ho la precedenza", NULL);
     }
 
-    //execl("/bin/ls", "ls", NULL);
     return 0; 
 }
