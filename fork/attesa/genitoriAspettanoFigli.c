91a77c8f8d261e00ea66b14a6fde99e0c6b4255d
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-01-04 11:04:41 +0100

    modficicata strutta fileystem

---
+++ fork/attesa/genitoriAspettanoFigli.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(){
+
+    printf("Programma dove i genitori aspettano i loro figli.\n\n");
+    
+    int pid = fork();
+    pid = fork();
+    pid = fork();
+
+    if(pid>0){
+        waitpid(pid, NULL, 0);
+        printf("[%d] Sono il processo padre di %d e l'ho aspettato\n", getpid(), pid);
+    }
+    else if(pid==0)
+        printf("[%d] Sono il processo figlio di %d e ho la precedenza\n", getpid(), getppid());
+    else if(pid==-1)
+        fprintf(stderr, "Errore fork\n");
+
+    return 0;
+
+}
