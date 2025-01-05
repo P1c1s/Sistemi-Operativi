391f49658716ff71d3d1df15017e82b276b11f6d
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-01-05 01:02:59 +0100

    aggiunti primi thread

---
+++ thread/presentazione/stampa.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM_THREADS 5
+
+int contatore = 0; // Contatore condiviso
+pthread_mutex_t mutex; // Mutex per proteggere l'accesso al contatore
+
+void* creazioneThread(void* arg){
+    pthread_mutex_lock(&mutex); // Acquisisce il mutex
+    contatore++;
+    pthread_mutex_unlock(&mutex); // Rilascia il mutex
+    printf("[PID %d] Thread %d\n", getpid(), contatore);
+    return NULL;
+}
+
+int main() {
+    
+    printf("Programma che crea N thread sempre nello stesso processo\n\n");
+
+    pthread_t threads[NUM_THREADS];
+    pthread_mutex_init(&mutex, NULL); // Inizializza il mutex
+
+    // Creazione dei thread
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, creazioneThread, NULL);
+    }
+
+    // Attesa della terminazione dei thread
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("\nEseguiti %d thred del processo %d\n", contatore, getpid());
+    pthread_mutex_destroy(&mutex); // Distrugge il mutex
+
+    return 0;
+}
