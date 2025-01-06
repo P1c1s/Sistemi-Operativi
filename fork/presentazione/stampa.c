560aa53072e18cc94881da455e559dfc0a99ab5f
Michela Sicuranza <michela.sicuranza@gmail.com>
2025-01-06 10:46:15 +0100

    correzione file

--- fork/presentazione/stampa.c
+++ fork/presentazione/stampa.c
@@ -5,13 +5,13 @@
 int main(){
 
     printf("Programma che in base al processo genitore/figlio stampa una cosa diversa.\n");
-    printf("| pid | processo |\n");
-    printf("------------------\n");
-    printf("| num | genitore |\n");
-    printf("|  0  |  figlio  |\n\n");
+    printf("|  PID  | tipo di processo |\n");
+    printf("----------------------------\n");
+    printf("|   0   |     genitore     |\n");
+    printf("|  num  |      figlio      |\n\n");
 
     int pid = fork();
-    printf("[%d] Stampa del valore di x: %d\n", pid, x);
+    printf("[PID %d] \n", pid);
 
     return 0;
 }
