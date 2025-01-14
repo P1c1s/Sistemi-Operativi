ec77722f5b648f7402d6ee21d8e51f4e4e3baf8e
Filomaior03 <filomaior12@gmail.com>
2025-01-14 11:26:38 +0100

    Modifica stampa.c

--- fork/presentazione/stampa.c
+++ fork/presentazione/stampa.c
@@ -4,14 +4,14 @@
 
 int main(){
 
-    printf("Programma che in base al processo genitore/figlio stampa una cosa diversa.\n");
+    printf("Programma che in base al processo genitore/figlio stampa una cosa diversa.\n\n");
     printf("|  PID  | tipo di processo |\n");
     printf("----------------------------\n");
     printf("|  num  |     genitore     |\n");
     printf("|   0   |      figlio      |\n\n");
 
     int pid = fork();
-    printf("[PID %d] \n", pid);
+    printf("[PID %d] \n\n", pid);
 
     return 0;
 }
