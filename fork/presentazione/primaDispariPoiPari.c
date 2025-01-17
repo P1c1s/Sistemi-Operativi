cc2825450249c8bd00567a045f652baff96ac1fa
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-01-17 11:11:56 +0100

    modifiche varie

--- fork/presentazione/primaDispariPoiPari.c
+++ fork/presentazione/primaDispariPoiPari.c
@@ -4,16 +4,19 @@
 #include<sys/wait.h>
 
 int main(){
-    int pidG = getpid();
 
-    int pid = fork();
+    printf("Programma in cui il processo genitore stampa numeri dispari\n");
+    printf("mentre il processo figlio stampa numeri pari.\n\n");
+
+    pid_t pidG = getpid();
+    pid_t pid = fork();
 
     for (int i=1; i<=10; i++){
         if(pid>0 && i%2 != 0){
-            printf("%d -> [genitore]\n", i);
+            printf("[genitore] -> %d\n", i);
         }
         else if(pid==0 && i%2 == 0){
-            printf("%d -> [figlio]\n", i);
+            printf("[figlio] -> %d\n", i);
             wait(&pidG);
         }
         else if(pid<0){
