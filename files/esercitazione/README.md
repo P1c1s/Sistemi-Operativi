e90fe43c94eef0920cc4f9e9f2d17c27132d70f7
Lorenzo Ricciardi <lorenzo.jones112@gmail.com>
2025-02-01 11:13:45 +0100

    aggiunti due files

---
+++ files/esercitazione/README.md
@@ -0,0 +1,5 @@
+#### Esericizio 1
+#### Copia file
+Scrivi un programma che crea un file di grandezza randomica - pieno caratteri ASCII - di circa 1000 bytes e effettua la copia del file generato il un secondo file.
+* Qualora il file da copiare abbia una grandezza minore o uguaule a 1000 bytes allora la copia può avvenire correttamente.
+* Nel caso il cui il file da copiare superi i 1000 bytes come grandezza, allora la copia non viene effetuata.
---
+++ files/esercitazione/esercizio1-lorenzo.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <time.h>
+
+#define FILE "originale.txt"
+
+int controlloGrandezza(){
+    char *carattere = (char*)malloc(sizeof(char));
+    int i = 1;
+    int fd = open(FILE, O_CREAT | O_RDONLY, S_IRWXU);
+    if(fd == -1){
+        fprintf(stderr, "Errore aperura file - controlloGrandezza()\n");
+        close(fd);
+    }
+    while(read(fd, carattere, sizeof(char)) > 0)
+        i++;
+    close(fd);
+    
+    if(i >= 1000)
+        printf("%s non copiato, grandezza file non supportata\n", FILE);
+
+    return i;
+}
+
+void copia(){
+    char* carattere = (char*)malloc(sizeof(char));
+    int fdOriginale = open(FILE, O_CREAT | O_RDWR, S_IRWXU);
+    int fdCopia = open("copia.txt", O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+    if(fdOriginale == -1){
+        fprintf(stderr, "Errore apertura fileOriginale - copia()\n");
+        close(fdOriginale);
+    }
+
+    if(fdCopia == -1){
+        fprintf(stderr, "Errore apertura fileCopia - copia()\n");
+        close(fdCopia);
+    }
+
+    while(read(fdOriginale, carattere, sizeof(char)) > 0){
+        write(fdCopia, carattere, sizeof(char));
+    }
+
+    close(fdOriginale);
+    close(fdCopia);
+    printf("%s copiato correttamente in copia.txt\n", FILE);
+}
+
+void generazione(){
+    int bytes = 10 * (rand()%500);
+    int fd = open(FILE, O_CREAT | O_WRONLY | O_TRUNC, S_IRWXU);
+
+    if(fd == -1){
+        fprintf(stderr, "Errore apertura file - generazione()\n");
+        close(fd);
+    }
+
+    for(int i=0; i<bytes; i++){
+        char carattere = 97 + rand()%24;
+        write(fd, &carattere, sizeof(char));
+    }
+    close(fd);
+    printf("Generato file da %d bytes\n", bytes);
+}
+
+int main(){
+
+    printf("Programma che \n\n");
+
+    srand(time(NULL));
+
+    generazione();
+
+    if(controlloGrandezza() <= 1000)
+        copia();
+
+    return 0;
+}
